f58b3c697adb95595110b23b6daf6a6ef171029c
Paulo <pauloh16.pdr@gmail.com>
2021-02-15 02:49:42 -0300

    Implementar range na paginacao dos itens

--- src/components/ProdutosPaginacao.vue
+++ src/components/ProdutosPaginacao.vue
@@ -1,6 +1,6 @@
 <template>
     <ul v-if="totalPaginas > 1">
-        <li v-for="pagina in totalPaginas" :key="pagina" >
+        <li v-for="pagina in paginas" :key="pagina" >
             <router-link :to="{ query: query(pagina) }">{{pagina}}</router-link>
         </li>
     </ul>
@@ -20,6 +20,22 @@
             }
         },
         computed: {
+            paginas() {
+                const paginaAtual = Number(this.$route.query._page);
+                const range = 9;
+                const offset = Math.ceil(range / 2);
+                const total = this.totalPaginas;
+                const listaPaginas = [];
+
+                for (let i = 1; i <= total; i++) {
+                    listaPaginas.push(i);
+                }
+
+                listaPaginas.splice(0, paginaAtual - offset);
+                listaPaginas.splice(range, total);
+
+                return listaPaginas;
+            },
             totalPaginas() {
                 const total = this.totalProdutos / this.produtosPorPagina;
                 return (total !== Infinity) ? Math.ceil(total) : 0;
