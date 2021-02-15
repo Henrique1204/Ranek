c4e0e93035c0198bf0538c67ad3e3d3fedeb2bca
Paulo <pauloh16.pdr@gmail.com>
2021-02-14 22:45:32 -0300

    Estilizar componente ProdutosLista

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,19 +1,25 @@
 <template>
-  <section>
-    <ul>
+  <div>
+    <ul v-if="produtos && produtos.length > 0">
       <li v-for="produto in produtos" :key="produto.id">
-        <img 
-          v-if="produto.fotos && produto.fotos.length"
-          :src="produto.fotos[0]"
-          :alt="produto.fotos[0].titulo"
-        >
+        <router-link to="/">
+          <img 
+            v-if="produto.fotos && produto.fotos.length"
+            :src="produto.fotos[0]"
+            :alt="produto.fotos[0].titulo"
+          >
 
-        <p class="preco">{{produto.preco}}</p>
-        <h2 class="titulo">{{produto.nome}}</h2>
-        <p>{{produto.descricao}}</p>
+          <p class="preco">{{produto.preco}}</p>
+          <h2>{{produto.nome}}</h2>
+          <p>{{produto.descricao}}</p>
+        </router-link>
       </li>
     </ul>
-  </section>
+
+      <p v-else-if="produtos && produtos.length === 0" class="sem_resultado">
+        Busca sem resultados. Tente buscar outro termo.
+      </p>
+  </div>
 </template>
 
 <script>
@@ -50,5 +56,52 @@
   };
 </script>
 
-<style>
+<style scoped>
+  div {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+
+  ul {
+    width: 100%;
+    margin: 30px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 30px;
+  }
+
+  li {
+    padding: 10px;
+    border-radius: 4px;
+    background-color: #FFF;
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+    transition: all 0.3s;
+  }
+
+  li:hover {
+    box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+    transform: scale(1.1);
+    position: relative;
+    z-index: 1;
+  }
+
+  li img {
+    border-radius: 4px;
+    margin-bottom: 20px;
+  }
+
+  li h2 {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .preco {
+    font-weight: bold;
+    color: #E80;
+  }
+
+  .sem_resultado {
+    text-align: center;
+  }
 </style>
