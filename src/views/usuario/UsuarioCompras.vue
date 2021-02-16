7b90fef24ade2dbf61490f012e2c29d8e7c8806a
Paulo <pauloh16.pdr@gmail.com>
2021-02-16 06:53:04 -0300

    Adicionar conteudo na tela UsuarioCompras

--- src/views/usuario/UsuarioCompras.vue
+++ src/views/usuario/UsuarioCompras.vue
@@ -1,12 +1,70 @@
 <template>
-  <p>Compras</p>
+  <div>
+    <h2>Compras</h2>
+
+    <transition mode="out-in">
+      <ul v-if="compras">
+        <li
+          class="produtos-wrapper"
+          v-for="({produto, vendedor_id}, index) in compras"
+          :key="`${index}`"
+        >
+          <ProdutoItem v-if="produto" :produto="produto">
+            <p class="vendedor"><span>Vendedor:</span> {{vendedor_id}}</p>
+          </ProdutoItem>
+        </li>
+      </ul>
+    </transition>
+  </div>
 </template>
 
 <script>
+  import ProdutoItem from '@/components/ProdutoItem.vue';
+  import { api } from '@/services.js';
+  import { mapState } from 'vuex';
+
   export default {
-    name: 'UsuarioCompras'
+    name: 'UsuarioCompras',
+    data: () => ({
+      compras: null,
+    }),
+    computed: {
+      ...mapState(['usuario', 'login'])
+    },
+    methods: {
+      getCompras() {
+        api.get(`/transacao?comprador_id=${this.usuario.id}`)
+        .then((res) => this.compras = res.data);
+      }
+    },
+    components: {
+      ProdutoItem
+    },
+    watch: {
+      login() {
+        this.getCompras();
+      }
+    },
+    created() {
+      if (this.login) {
+        this.getCompras();
+      }
+    }
   };
 </script>
 
-<style>
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  .produto-wrapper {
+    margin-bottom: 40px;
+  }
+
+  .vendedor span {
+    color: #E80;
+  }
 </style>
